dbe0b8a96fa4c33b5f39cbcafc9e629b90cd4f32
fan <fanziqi@creality.com>
2026-08-16 21:56:55 +0800

    day1:环境搭建与基础运算练习

---
+++ day1_demo.py
@@ -0,0 +1,19 @@
+# Day1 基础运算练习
+num1 = 20
+num2 = 7
+
+add = num1 + num2
+sub = num1 - num2
+mul = num1 * num2
+div = num1 / num2    # 普通除法，结果浮点数
+div_floor = num1 // num2  # 向下取整除法
+mod = num1 % num2    # 取余
+power = num1 ** 2    # 平方
+
+print("加法：", add)
+print("减法：", sub)
+print("乘法：", mul)
+print("普通除法：", div)
+print("取整除法：", div_floor)
+print("取余数：", mod)
+print("平方：", power)
